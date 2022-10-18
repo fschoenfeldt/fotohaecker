ba7e14056cf3fba3627e491cf05d3358fecf10d9
Frederik Schönfeldt <frederikschoenfeldt@gmail.com>
2022-10-18 12:47:19 +0200

    improve language select & refactor

--- lib/fotohaecker_web/live/index_live/home.ex
+++ lib/fotohaecker_web/live/index_live/home.ex
@@ -44,56 +44,66 @@ defmodule FotohaeckerWeb.IndexLive.Home do
       <h1 class="">
         <%= gettext("Upload your photos, license-free.") %>
       </h1>
-      <.form
-        :let={f}
-        for={@photo_changeset}
-        multipart={true}
-        phx-submit="submission_submit"
-        phx-change="submission_change"
-        class="upload_form"
-      >
-        <div>
-          <label for="photo_title">
-            <%= gettext("title") %>
-          </label>
-          <input
-            id="photo_title"
-            type="text"
-            name="photo[title]"
-            placeholder={gettext("photo title")}
-            required
-            value={@submission_params.title}
-            phx-debounce="blur"
-          />
-          <%= error_tag(f, :title) %>
-        </div>
-        <div phx-drop-target={@uploads.photo.ref} class="dropzone">
-          <div class="dark:text-gray-200">
-            <%= gettext("drag the photo here or use the file button below") %>
-          </div>
-          <.live_file_input upload={@uploads.photo} />
-          <%= for entry <- @uploads.photo.entries do %>
-            <%= unless Enum.empty?(upload_errors(@uploads.photo, entry)) do %>
-              <div hidden>
-                <%= inspect(entry) %>
-              </div>
-              <div class="">
-                <div class="text-red-500"><%= gettext("the selected file can't be uploaded:") %></div>
-
-                <ul class="list-disc list-inside">
-                  <%= for err <- upload_errors(@uploads.photo, entry) do %>
-                    <li class="text-red-500"><%= error_to_string(err) %></li>
-                  <% end %>
-                </ul>
-              </div>
-            <% end %>
-          <% end %>
-        </div>
-        <button class="btn btn--green" type="submit" phx-disable-with={gettext("uploading..")}>
-          <%= gettext("upload") %>
-        </button>
-      </.form>
+      <.upload_form
+        photo_changeset={@photo_changeset}
+        submission_params={@submission_params}
+        uploads={@uploads}
+      />
     </div>
+    """
+  end
+
+  defp upload_form(assigns) do
+    ~H"""
+    <.form
+      :let={f}
+      for={@photo_changeset}
+      multipart={true}
+      phx-submit="submission_submit"
+      phx-change="submission_change"
+      class="upload_form"
+    >
+      <div>
+        <label for="photo_title">
+          <%= gettext("title") %>
+        </label>
+        <input
+          id="photo_title"
+          type="text"
+          name="photo[title]"
+          placeholder={gettext("photo title")}
+          required
+          value={@submission_params.title}
+          phx-debounce="150"
+        />
+        <%= error_tag(f, :title) %>
+      </div>
+      <div phx-drop-target={@uploads.photo.ref} class="dropzone">
+        <div class="dark:text-gray-200" aria-hidden="true">
+          <%= gettext("drag the photo here or use the file button below") %>
+        </div>
+        <.live_file_input upload={@uploads.photo} />
+        <%= for entry <- @uploads.photo.entries do %>
+          <%= unless Enum.empty?(upload_errors(@uploads.photo, entry)) do %>
+            <div hidden>
+              <%= inspect(entry) %>
+            </div>
+            <div class="">
+              <div class="text-red-500"><%= gettext("the selected file can't be uploaded:") %></div>
+
+              <ul class="list-disc list-inside">
+                <%= for err <- upload_errors(@uploads.photo, entry) do %>
+                  <li class="text-red-500"><%= error_to_string(err) %></li>
+                <% end %>
+              </ul>
+            </div>
+          <% end %>
+        <% end %>
+      </div>
+      <button class="btn btn--green" type="submit" phx-disable-with={gettext("uploading..")}>
+        <%= gettext("upload") %>
+      </button>
+    </.form>
     """
   end
 
