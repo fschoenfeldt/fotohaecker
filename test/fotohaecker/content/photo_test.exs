4a9e7972565e984e83ce50e8dcf792836c14629a
Frederik Schönfeldt <frederikschoenfeldt@gmail.com>
2022-11-13 13:20:14 +0100

    now stripping metadata by using sharp on the original photo, also og photo isn't as big anymore

---
+++ test/fotohaecker/content/photo_test.exs
@@ -0,0 +1,11 @@
+defmodule Fotohaecker.PhotoTest do
+  use Fotohaecker.DataCase
+
+  alias Fotohaecker.Content.Photo
+  doctest Fotohaecker.Content.Photo
+
+  test "gen_path/1 returns path for saving a photo" do
+    assert Photo.gen_path("test.jpg") =~
+             "/lib/fotohaecker/priv/static/images/uploads/test.jpg"
+  end
+end
