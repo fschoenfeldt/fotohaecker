cb23d3ea42daddef1b0dcdec18a69037583b2ac4
Frederik Schönfeldt <frederikschoenfeldt@gmail.com>
2023-06-07 15:16:39 +0200

    update deploy script hotfix

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,5 +6,7 @@ rsync --archive --compress --progress --partial --human-readable \
       $(pwd)/_uberspace/fotohaeckertwo.ini \
       $UBERSPACE_USER@$UBERSPACE_SERVER:/home/$UBERSPACE_USER/etc/services.d/fotohaeckertwo.ini
 
-ssh -t uberspace 'cd fotohaecker && mix deps.get --only prod && MIX_ENV="prod" mix phx.digest.clean && MIX_ENV=prod mix assets.setup && MIX_ENV=prod mix assets.deploy'
+# TODO: find a better way to do this
+# somehow, pnpm doesn't get recognized when using with mix
+ssh -t uberspace 'cd fotohaecker && mix deps.get --only prod && MIX_ENV="prod" mix phx.digest.clean && npm --prefix assets install && MIX_ENV=prod mix assets.deploy'
 ssh -t uberspace 'supervisorctl reread && supervisorctl update && supervisorctl restart fotohaeckertwo'
