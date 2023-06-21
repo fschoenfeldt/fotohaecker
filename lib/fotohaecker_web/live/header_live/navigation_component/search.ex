3a745ff97b1988676b7d7657541db7676e912ddd
Frederik Schönfeldt <frederikschoenfeldt@gmail.com>
2023-06-21 14:22:45 +0200

    refactor and update gettext

--- lib/fotohaecker_web/live/header_live/navigation_component/search.ex
+++ lib/fotohaecker_web/live/header_live/navigation_component/search.ex
@@ -23,65 +23,86 @@ defmodule FotohaeckerWeb.HeaderLive.NavigationComponent.Search do
         phx-change="search"
         phx-target={@myself}
       >
-        <label class="sr-only" for="search_query">
-          <%= gettext("Search by title or tag") %>
-        </label>
-        <input
-          class={[
-            "w-full bg-transparent rounded text-white placeholder:text-gray-400 border-transparent",
-            @search_query !== "" && "rounded-b-none"
-          ]}
-          type="text"
-          value={@search_query}
-          phx-debounce="500"
-          placeholder="Search..."
-          name="search_query"
-          id="search_query"
-        />
-        <button :if={@search_query !== ""} type="submit" class="border-none" phx-target={@myself}>
-          <Heroicons.arrow_right class="w-4 h-4 stroke-gray-200" alt="" />
-          <span class="sr-only"><%= gettext("submit") %></span>
-        </button>
-        <button
-          :if={@search_query !== ""}
-          type="button"
-          class="border-none"
-          phx-click="search_reset"
-          phx-target={@myself}
-        >
-          <Heroicons.x_mark class="w-4 h-4 stroke-gray-200" alt="" />
-          <span class="sr-only"><%= gettext("clear search") %></span>
-        </button>
+        <.search_input search_query={@search_query} />
+        <.search_submit_or_cancel myself={@myself} search_query={@search_query} />
+        <.search_results search_results={@search_results} />
       </.form>
-      <div
-        class={[
-          "bg-gray-800 border border-gray-700 border-t-transparent rounded-b absolute top-[calc(2.5rem+4px)] z-10 w-full hidden",
-          @search_results && "!block"
-        ]}
-        aria-live="polite"
-      >
-        <%= if (@search_results) && (length(@search_results) > 0) do %>
-          <div class="text-gray-200 p-2">
-            <%= ngettext("1 result", "%{count} results", length(@search_results)) %>
-          </div>
-          <ul class="flex flex-col divide-y divide-gray-700" data-testid="result_list">
-            <span class="sr-only"><%= gettext("search results") %></span>
-            <li
-              :for={photo <- @search_results}
-              class="p-2 text-gray-200 underline hover:text-gray-100 hover:bg-gray-700 cursor-pointer group"
-            >
-              <.link class="link link--light" href={photo_route(photo.id)}>
-                <%= photo.title %>
-              </.link>
-            </li>
-          </ul>
-        <% end %>
-        <%= if (@search_results) && (length(@search_results) === 0) do %>
-          <div class="text-gray-200 p-2">
-            <%= gettext("No results") %>
-          </div>
-        <% end %>
-      </div>
+    </div>
+    """
+  end
+
+  defp search_submit_or_cancel(assigns) do
+    ~H"""
+    <button
+      :if={@search_query !== ""}
+      type="submit"
+      class="btn btn--dark hover:bg-gray-700 border-none"
+      phx-target={@myself}
+    >
+      <Heroicons.arrow_right class="w-4 h-4 stroke-gray-200" alt="" />
+      <span class="sr-only"><%= gettext("submit") %></span>
+    </button>
+    <button
+      :if={@search_query !== ""}
+      type="button"
+      class="btn btn--dark hover:bg-gray-700 border-none"
+      phx-click="search_reset"
+      phx-target={@myself}
+    >
+      <Heroicons.x_mark class="w-4 h-4 stroke-gray-200" alt="" />
+      <span class="sr-only"><%= gettext("clear search") %></span>
+    </button>
+    """
+  end
+
+  defp search_input(assigns) do
+    ~H"""
+    <label class="sr-only" for="search_query">
+      <%= gettext("Search by title or tag") %>
+    </label>
+    <input
+      class={[
+        "w-full bg-transparent rounded text-white placeholder:text-gray-400 border-transparent",
+        @search_query !== "" && "rounded-b-none"
+      ]}
+      type="text"
+      value={@search_query}
+      phx-debounce="500"
+      placeholder="Search..."
+      name="search_query"
+      id="search_query"
+    />
+    """
+  end
+
+  defp search_results(assigns) do
+    ~H"""
+    <div
+      class={[
+        "bg-gray-800 border border-gray-700 border-t-transparent rounded-b absolute top-[calc(2.5rem+4px)] z-10 w-full hidden",
+        @search_results && "!block"
+      ]}
+      aria-live="polite"
+    >
+      <%= if (@search_results) && (length(@search_results) > 0) do %>
+        <div class="text-gray-200 p-2">
+          <%= ngettext("1 result", "%{count} results", length(@search_results)) %>
+        </div>
+        <%!-- # TODO: refactor this and use photos_component.ex  --%>
+        <ul class="flex flex-col divide-y divide-gray-700" data-testid="result_list">
+          <span class="sr-only"><%= gettext("search results") %></span>
+          <li :for={photo <- @search_results} class="hover:bg-gray-700">
+            <.link class="p-2 block h-full w-full link link--light" href={photo_route(photo.id)}>
+              <%= photo.title %>
+            </.link>
+          </li>
+        </ul>
+      <% end %>
+      <%= if (@search_results) && (length(@search_results) === 0) do %>
+        <div class="text-gray-200 p-2">
+          <%= gettext("No results") %>
+        </div>
+      <% end %>
     </div>
     """
   end
