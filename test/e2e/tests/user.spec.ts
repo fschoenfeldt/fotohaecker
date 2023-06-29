fb9be744da4d1e96ceaab1d11ef195e2f0d877b0
Frederik Schönfeldt <frederikschoenfeldt@gmail.com>
2023-06-29 16:30:42 +0200

    add poc test that probably wont be used

--- test/e2e/tests/user.spec.ts
+++ test/e2e/tests/user.spec.ts
@@ -31,3 +31,30 @@ test.describe("User Settings page", () => {
     await expect(page.locator("a", { hasText: "login" })).toBeVisible();
   });
 });
+
+// TODO: I'm not sure if we should test this here
+// test.describe.serial("Register / Delete User", () => {
+//   test.beforeEach(async ({ page }) => {
+//     await page.goto("/fh/auth/logout");
+//   });
+
+//   test("can register a new user", async ({ page, context }) => {
+//     const alert = page.locator(".alert--info");
+//     await expect(alert).toContainText("logged out");
+//     await alert.click();
+//     // clear cookies
+//     await context.clearCookies();
+//     const loginLink = page.locator("a", { hasText: "login" });
+//     await loginLink.click();
+//     await page.locator("a", { hasText: "Sign up" }).click();
+
+//     await page.locator("#email").fill("testuser@example.com");
+//     await page.locator("#password").fill("Testuser1234");
+//     await page.locator("#password").press("Enter");
+//     await page.locator("button", { hasText: "Accept" }).click();
+//     await expect(alert).toContainText("authenticated");
+//     await page.goto("/fh/en_US/user");
+//     await page.locator("button", { hasText: "Delete Account" }).click();
+//     await expect(alert).toContainText("Your account has been deleted!");
+//   });
+// });
