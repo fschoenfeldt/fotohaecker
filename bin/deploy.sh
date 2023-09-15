6ba4f7b75bd0dd5b3fa7cdf3d4b4b49f83387a98
Frederik Schönfeldt <frederikschoenfeldt@gmail.com>
2023-09-15 12:24:29 +0200

    improve deployment script with uberspace specifics

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -8,5 +8,5 @@ rsync --archive --compress --progress --partial --human-readable \
 
 # TODO: find a better way to do this
 # somehow, pnpm doesn't get recognized when using with mix
-ssh -t uberspace 'cd fotohaecker && mix deps.get --only prod && MIX_ENV="prod" mix phx.digest.clean && npm --prefix assets install && MIX_ENV=prod mix assets.deploy'
+ssh -t uberspace 'cd fotohaecker && EXQLITE_USE_SYSTEM=1 EXQLITE_SYSTEM_CFLAGS="-I/usr/include" EXQLITE_SYSTEM_LDFLAGS="-L/lib64/sqlite -lsqlite3" mix deps.get --only prod && EXQLITE_USE_SYSTEM=1 EXQLITE_SYSTEM_CFLAGS="-I/usr/include" EXQLITE_SYSTEM_LDFLAGS="-L/lib64/sqlite -lsqlite3" MIX_ENV=prod mix compile && MIX_ENV="prod" mix phx.digest.clean && npm --prefix assets install && MIX_ENV=prod mix assets.deploy'
 ssh -t uberspace 'supervisorctl reread && supervisorctl update && supervisorctl restart fotohaeckertwo'
