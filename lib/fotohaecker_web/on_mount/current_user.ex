d9866bbb2bdec9e1ce8d6cfa792063be34c32eec
Frederik Schönfeldt <frederikschoenfeldt@gmail.com>
2023-10-11 12:38:17 +0200

    fix error when user is in session despite user management not being enabled

--- lib/fotohaecker_web/on_mount/current_user.ex
+++ lib/fotohaecker_web/on_mount/current_user.ex
@@ -2,10 +2,17 @@ defmodule FotohaeckerWeb.OnMount.CurrentUser do
   @moduledoc """
   Ensures that the current user is available in the socket assigns.
   """
+  require Logger
 
   def on_mount(:default, _params, %{"current_user" => user} = _session, socket) do
-    {:ok, user} = Fotohaecker.UserManagement.get(user.id)
-    {:cont, Phoenix.Component.assign(socket, :current_user, user)}
+    case Fotohaecker.UserManagement.get(user.id) do
+      {:ok, user} ->
+        {:cont, Phoenix.Component.assign(socket, :current_user, user)}
+
+      _other ->
+        Logger.warning("OnMount.CurrentUser: session user with id #{user.id} not found.")
+        {:cont, Phoenix.Component.assign(socket, :current_user, nil)}
+    end
   end
 
   def on_mount(:default, _params, _session, socket) do
