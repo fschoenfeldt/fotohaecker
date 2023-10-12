0d731965083a45f06e24d7a53535ee45fcbe9668
Frederik Schönfeldt <frederikschoenfeldt@gmail.com>
2023-10-12 15:02:38 +0200

    fix callback on prod

--- config/runtime.exs
+++ config/runtime.exs
@@ -82,7 +82,7 @@ if config_env() == :prod do
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :fotohaecker, FotohaeckerWeb.Endpoint,
-    url: [host: host, scheme: "https"],
+    url: [host: host, scheme: "https", port: 443],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
